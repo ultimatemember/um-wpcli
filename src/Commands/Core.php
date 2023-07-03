345fc84140eae815956ac61f0d1efebb0d052c5e
Champ Camba <champsupertramp@users.noreply.github.com>
2023-07-04 02:21:23 +0800

    Add command to retrieve flagged accounts count

--- src/Commands/Core.php
+++ src/Commands/Core.php
@@ -22,6 +22,7 @@ class Core {
 		if ( defined( 'WP_CLI' ) && \WP_CLI ) {
 			\WP_CLI::add_command( 'um security test', array( $this, 'test_security_settings' ) );
 			\WP_CLI::add_command( 'um security affected users', array( $this, 'test_security_settings_affected_users' ) );
+			\WP_CLI::add_command( 'um security flagged users', array( $this, 'test_security_settings_total_flagged_accounts' ) );
 		}
 
 	}
@@ -31,7 +32,9 @@ class Core {
 	 * Command: wp um security test user=<user_id>
 	 */
 	public function test_security_settings( $args, $assoc_args ) {
-		$args = wp_parse_args( $args[0] );
+		$args = wp_parse_args( $args[0], array(
+			'user' => null,
+		) );
 		\WP_CLI::success( esc_html__( 'Account with user ID ' . $args['user'] . ' has been validated. ', 'ultimate-member' ) );
 		
 		add_filter( 'um_secure_blocked_user_redirect_immediately', '__return_false' );
@@ -71,7 +74,77 @@ class Core {
 				\WP_CLI::success( \WP_CLI::colorize( esc_html__( '`%Y' . $cap . '`%N', 'ultimate-member' ) ) . ' has ' . \WP_CLI::colorize( esc_html__('%Raffected `' . $count_users . '`%N user accounts. ', 'ultimate-member' ) ) );
 			}
 		}
+	}
+
+	/**
+	 * Total Flagged accounts
+	 * Command: wp um security flagged users interval=<today|last_hour>
+	 */
+	public function test_security_settings_total_flagged_accounts( $args, $assoc_args ) {
+		$args = wp_parse_args( isset( $args[0] ) ? $args[0]: '', array(
+			'interval' => '',
+		) );
+
+		if ( 'today' === $args['interval'] ) {
+			$query_args = array(
+				'fields'     => 'ID',
+				'relation'   => 'AND',
+				'meta_query' => array(
+					'relation' => 'AND',
+					array(
+						'key'     => 'um_user_blocked__datetime',
+						'value'   => gmdate( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+						'compare' => '>=',
+						'type'    => 'DATE',
+					),
+					array(
+						'key'     => 'um_user_blocked__datetime',
+						'value'   => gmdate( 'Y-m-d H:i:s', strtotime( 'now' ) ),
+						'compare' => '<=',
+						'type'    => 'DATE',
+					),
+				),
+			);
+			$users = new \WP_User_Query( $query_args );
+
+			\WP_CLI::success( \WP_CLI::colorize( sprintf( _n( 'There\s %d account that has been blocked today.', 'There are %d accounts that have been blocked today.', $users->get_total(), 'ultimate-member' ), $users->get_total() ) ) );
+			
+		} else if( 'last_hour' == $args['interval'] ) {
+			$query_args = array(
+				'fields'     => 'ID',
+				'meta_query' => array(
+					'relation' => 'AND',
+					array(
+						'key'     => 'um_user_blocked__datetime',
+						'value'   => gmdate( 'Y-m-d H:i:s', strtotime( '-1 hour' ) ),
+						'compare' => '>=',
+						'type'    => 'DATETIME',
+					),
+				),
+			);
+
+			$users = new \WP_User_Query( $query_args );
+
+			\WP_CLI::success( \WP_CLI::colorize( sprintf( _n( 'There\s %d account that has been blocked within the last hour.', 'There are %d accounts that have been blocked within the last hour.', $users->get_total(), 'ultimate-member' ), $users->get_total() ) ) );
+			
+		} else if ( empty( $args['interval'] ) ) {
+			$query_args = array(
+				'fields'     => 'ID',
+				'relation'   => 'AND',
+				'meta_query' => array(
+					'relation' => 'AND',
+					array(
+						'key'     => 'um_user_blocked__datetime',
+						'compare' => 'EXISTS',
+					),
+					
+				),
+			);
+			$users = new \WP_User_Query( $query_args );
+			\WP_CLI::success( \WP_CLI::colorize( sprintf( _n( 'There\s %d account that has been blocked on your site.', 'There are %d accounts that have been blocked on your site.', $users->get_total(), 'ultimate-member' ), $users->get_total() ) ) );
 		
+		}
+
 		
 	}
 }
